67546eee3cdbcd05a01ecccb6e9913c2a22a4806
Ei-Thinzar <eimaung20022004@gmail.com>
2025-12-31 13:44:58 +0630

    admin user middleware add

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration {
             $table->string('password');
             $table->string('phone');
             $table->string('address');
+            $table->string('role')->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/api.php
+++ routes/api.php
@@ -9,19 +9,22 @@ use App\Http\Controllers\AuthController;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
+//All users and admin
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/products', [ProductController::class, 'index']);
-    Route::post('/products', [ProductController::class, 'store']);
-
     Route::get('/categories', [CategoryController::class, 'index']);
-    Route::post('/categories', [CategoryController::class, 'store']);
-
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    Route::get('/users', [AuthController::class, 'index']);
+
+    //only Admin
+    Route::middleware('admin')->group(function () {
+        Route::post('/products', [ProductController::class, 'store']);
+        Route::post('/categories', [CategoryController::class, 'store']);
+        Route::get('/users', [AuthController::class, 'index']);
+    });
 });
 
 
